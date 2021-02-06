3be032499a20b0ac7962e4ba4af091016de35385
Dlarsen93 <dlarsen93@byui.edu>
2021-02-06 10:02:17 -0700

    week5.2

--- js/main.js
+++ js/main.js
@@ -24,36 +24,17 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 }})
 
-/*const links = [
-    {
-        label: "Notes",
-        url: "Week1/index.html",
-        weekHeading: "Week 1"
-    },
-    {
-        label: "Week 2 notes",
-        url: "week2/notes.html"
-    },
-    {
-        label: "Week 3 notes",
-        url: "week3/apps.html"
-    },
-];
+var coll = document.getElementsByClassName("collapsible");
+var i;
 
-const ol = document.querySelector('ol');
-
-for (const item of links) {
-    const heading = document.createElement('h3');
-    const li = document.createElement('li');
-    const a = document.createElement('a');
-    li.setAttribute('href', item.url);
-    a.textContent = item.label;
-    //heading.textContent = item.lebel;
-
-    //h3.appendChild(h3);
-    li.appendChild(a);
-    ol.appendChild(li);
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
 }
-
-var lastEdit = document.lastModified;
-document.getElementById("lastModified").textContent = lastEdit;*/
